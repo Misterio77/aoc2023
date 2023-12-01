db0e242c975d0265c7812fb692b6781fe5247de4
Gabriel Fontes <hi@m7.rs>
2023-12-01 17:32:01 -0300

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+[![Hydra Status](https://img.shields.io/endpoint?url=https://hydra.m7.rs/job/aoc2023/main/x86_64-linux.default/shield)](https://hydra.m7.rs/jobset/aoc2023/main#tabs-jobs)
+
+# Advent of Code 2022
+
+My attempt on [AoC 2023](https://adventofcode.com/2023).
+
+## How to run
+
+Just use Nix:
+
+```
+nix run .#day1
+```
+
+## How to hack
+
+Believe it or not, also Nix:
+
+```
+nix develop .#day1
+```
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Advent of Code 2023";
+
+  nixConfig = {
+    extra-substituters = ["https://cache.m7.rs"];
+    extra-trusted-public-keys = ["cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default-linux";
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.systems.follows = "systems";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    systems,
+    ...
+  }: let
+    inherit (builtins) filter attrNames readDir;
+    inherit (nixpkgs.lib) hasPrefix genAttrs;
+    inherit (utils.lib) filterPackages eachSystem;
+    days = filter (hasPrefix "day") (attrNames (readDir ./.));
+  in
+    eachSystem (import systems) (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages = genAttrs days (day: pkgs.callPackage ./${day}/default.nix);
+      devShells = genAttrs days (day: pkgs.callPackage ./${day}/shell.nix);
+      hydraJobs = filterPackages system packages;
+      formatter = pkgs.alejandra;
+    });
+}
